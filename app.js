4cbec15fcd25532ec316b0c90257ba71315a2506
Geanina Hinta <geaninahinta@yahoo.com>
2016-02-28 22:52:37 +0000

    html
    Connect with a html page

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/hello', function(request, response) {
-    response.send('Hello World!');
+    response.sendFile('/Development/Nodejs/index.html');
 });
 
+app.use('/assets', express.static('assets'));
 app.listen(process.env.PORT || 777);
