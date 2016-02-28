4e45e6981c4e6c1fa4976cea20ff32974e333501
Geanina Hinta <geaninahinta@yahoo.com>
2016-02-28 23:09:15 +0000

    Add photo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/hello', function(request, response) {
-    response.sendFile('/Development/Nodejs/index.html');
+    response.sendFile(__dirname + '/index.html');
 });
 
 app.use('/assets', express.static('assets'));
