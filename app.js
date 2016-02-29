d85852b3b13c01df7bcccdf033be09ade9fd4bf6
Geanina Hinta <geaninahinta@yahoo.com>
2016-02-29 00:53:46 +0000

    Rectangle
    Add an awesome blue and red rectangle

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ app.get('/hello', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/game.html');
+});
+
 app.use('/assets', express.static('assets'));
+
 app.listen(process.env.PORT || 777);
---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var screen = document.getElementById('screen');
+var context = screen.getContext('2d');
+
+context.fillStyle = '#ffffff';
+context.fillRect(0, 0, 1280, 720);
+
+context.fillStyle = '#0000ee';
+context.fillRect(490, 210, 300, 300);
+
+context.fillStyle = '#ee0000';
+context.fillRect(590, 310, 100, 100);
+
+
