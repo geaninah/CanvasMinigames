9387491f7ffde2c7b1bd89dd803c96607f348bf0
Geanina Hinta <geaninahinta@yahoo.com>
2016-03-04 21:33:34 +0000

    Win alert box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,34 @@
 var screen = document.getElementById('screen');
 var context = screen.getContext('2d');
 
-screen.width = window.innerWidth * 0.6;
+screen.width = window.innerWidth * 0.4;
 screen.height = window.innerHeight * 0.6;
 
+var rows;
+var columns;
 var tiles;
 
-var generateTiles = function(rows, columns){
-    var tiles = [];
+var generateTiles = function(){
+    tiles = [];
 
     for(var row = 0; row < rows; row++){
         tiles.push([]);
-
+        
         for(var column = 0; column < columns; column++){
-            var random = Math.random();
-
-            if(random < 0.5){
-                tiles[row].push(0);
-            }else{
-                tiles[row].push(1);
-            }
+            tiles[row].push(0);
         }
     }
 
-    return tiles;
+    for(var index = 0; index < 7; index++){
+        changeTiles(Math.floor(Math.random() * rows), 
+            Math.floor(Math.random() * columns));
+    }
 };
 
 var changeState = function(row, column){
     if(tiles[row][column] == 0){
         tiles[row][column] = 1;
-    }
-    else{
+    }else{
         tiles[row][column] = 0;
     }
 }
@@ -39,21 +37,23 @@ var changeTiles = function(row, column){
     if(column > 0){
         changeState(row, column - 1);
     }
+
     if(column < tiles[0].length - 1){
         changeState(row, column + 1);
     }
+    
     if(row > 0){
         changeState(row - 1, column);
     }
+    
     if(row < tiles.length - 1){
         changeState(row + 1, column);
     }
+    
     changeState(row, column);
 }
 
 var drawTiles = function(){
-    var rows = tiles.length;
-    var columns = tiles[0].length;
     var padding = 5;
 
     // Calculates the width of a tile
@@ -91,33 +91,57 @@ var draw = function(){
     drawTiles();
 };
 
-var initializeGame = function(rows, columns){
-    tiles = generateTiles(rows, columns);
+var initializeGame = function(noOfRows, noOfColumns){
+    rows = noOfRows;
+    columns = noOfColumns;
+    generateTiles();
     draw();
 };
 
-var noOfRows = 4;
-var noOfColumns = 3;
-initializeGame(noOfRows, noOfColumns);
+var hasWon = function(){
+    var firstCheck = tiles[0][0];
 
-screen.addEventListener('click', function(e) {
-    var clickedRow = Math.floor(getY(e) / (screen.height / noOfRows));
-    var clickedColumn = Math.floor(getX(e) / (screen.width / noOfColumns));
+    for(var row = 0; row < rows; row++){
+        for(var column = 0; column < columns; column++){
+            if(tiles[row][column] != firstCheck){
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
+
+var noOfClicks = 0;
+
+screen.addEventListener('click', function(e){
+    var clickedRow = Math.floor(getY(e) / (screen.height / rows));
+    var clickedColumn = Math.floor(getX(e) / (screen.width / columns));
+    
     changeTiles(clickedRow, clickedColumn);
-    drawTiles();
+    draw();
+
+    noOfClicks++;
+
+    if(hasWon()){
+        alert("You won! Number of clicks: " + noOfClicks);
+        initializeGame(rows, columns);
+        noOfClicks = 0;
+    }
 });
 
-var getX = function(e) {
+var getX = function(e){
     var rect = screen.getBoundingClientRect();
     var relativeX = e.clientX - rect.left;
 
     return relativeX;
 };
 
-var getY = function(e) {
+var getY = function(e){
     var rect = screen.getBoundingClientRect();
     var relativeY = e.clientY - rect.top;
 
     return relativeY;
 };
 
+initializeGame(3, 4);
